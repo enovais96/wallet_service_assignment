04e101e7e6517da26417a907878deade25ea051f
Eduardo Novais <eduardo.novais96@hotmail.com>
2024-09-04 21:40:54 -0300

    [NEW] - Making adjustments in the user flow

--- user-service/src/main/java/com/recargapay/user_service/controller/UserController.java
+++ user-service/src/main/java/com/recargapay/user_service/controller/UserController.java
@@ -3,12 +3,15 @@ package com.recargapay.user_service.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
 import com.recargapay.user_service.model.User;
+import com.recargapay.user_service.model.ErrorResponse;
 import com.recargapay.user_service.repository.UserRepository;
 import com.recargapay.user_service.exception.ResourceNotFoundException;
 
 import java.util.Map;
 import java.util.HashMap;
+import java.util.Optional;
 
 import java.util.List;
 
@@ -18,34 +21,57 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
+    @GetMapping("/{idUser}")
+    public ResponseEntity<?> getUser(@PathVariable Long idUser) {
+        Optional<User> user = userRepository.findById(idUser);
+        if (user.isPresent()) {
+            return ResponseEntity.ok(user.get());
+        } else {
+            ErrorResponse errorResponse = new ErrorResponse("User not found for this idUser :: " + idUser);
+            return ResponseEntity.ok(errorResponse);
+        }
+    }
+
     @GetMapping
     public List<User> getAllUsers() {
         return userRepository.findAll();
     }
 
     @PostMapping
-    public User createUser(@RequestBody User user) {
-        return userRepository.save(user);
+    public ResponseEntity<?>  createUser(@RequestBody User user) {
+        if (userRepository.findByNameUser(user.getNameUser()).isPresent()) {
+            return ResponseEntity.status(HttpStatus.CONFLICT)
+                    .body("A user with the same name already exists.");
+        }
+        User savedUser = userRepository.save(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedUser);
     }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<User> updateUser(@PathVariable Long idUser, @RequestBody User userDetails) {
-        User user = userRepository.findById(idUser)
-                .orElseThrow(() -> new ResourceNotFoundException("User not found for this idUser :: " + idUser));
-        
-        user.setNameUser(userDetails.getNameUser());
-        final User updatedUser = userRepository.save(user);
-        return ResponseEntity.ok(updatedUser);
+    @PutMapping("/{idUser}")
+    public ResponseEntity<?> updateUser(@PathVariable Long idUser, @RequestBody User userDetails) {
+        Optional<User> user = userRepository.findById(idUser);
+        if (user.isPresent()) {
+            user.get().setNameUser(userDetails.getNameUser());
+            final User updatedUser = userRepository.save(user.get());
+            return ResponseEntity.ok(updatedUser);
+        } else {
+            ErrorResponse errorResponse = new ErrorResponse("User not found for this idUser :: " + idUser);
+            return ResponseEntity.ok(errorResponse);
+        }
     }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Map<String, Boolean>> deleteUser(@PathVariable Long idUser) {
-        User user = userRepository.findById(idUser)
-                .orElseThrow(() -> new ResourceNotFoundException("User not found for this idUser :: " + idUser));
+    @DeleteMapping("/{idUser}")
+    public ResponseEntity<?> deleteUser(@PathVariable Long idUser) {
+        Optional<User> user = userRepository.findById(idUser);
+        if (user.isPresent()) {
+            userRepository.delete(user.get());
+            Map<String, Boolean> response = new HashMap<>();
+            response.put("deleted", Boolean.TRUE);
 
-        userRepository.delete(user);
-        Map<String, Boolean> response = new HashMap<>();
-        response.put("deleted", Boolean.TRUE);
-        return ResponseEntity.ok(response);
+            return ResponseEntity.ok(response);
+        } else {
+            ErrorResponse errorResponse = new ErrorResponse("User not found for this idUser :: " + idUser);
+            return ResponseEntity.ok(errorResponse);
+        }
     }
 }
--- user-service/src/main/java/com/recargapay/user_service/repository/UserRepository.java
+++ user-service/src/main/java/com/recargapay/user_service/repository/UserRepository.java
@@ -3,8 +3,9 @@ package com.recargapay.user_service.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.recargapay.user_service.model.User;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    // Métodos adicionais, se necessários
+    Optional<User> findByNameUser(String nameUser);
 }
